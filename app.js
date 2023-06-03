ddf972facaadd7e1c0f51e19b296dd3c0e627621
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-06-03 19:21:10 +0500

    90% compeleted

--- app.js
+++ app.js
@@ -1,61 +1,243 @@
-// function addtodo() {
-// var todosinput = document.getElementById("inpt-todo")
-// var list = document.getElementById("list")
-//  list.innerHTML+=  `<li>${todosinput.value}</li> <button onlclick = "deltodo" >Delete</button><button  onlclick = "edttodo" >edit</button>`
-// todosinput.value = ""
+var watch = {
+    tagheuer: {
+        tagheuer_model_1: {
+            model: "TAG Heuer AQUARACER ALARM WBP1110.BA0627",
+            price: 614_500,
+            img: "images/tag-watch-1.jpg",
+            company: "tagheuer"
+        },
+        tagheuer_model_2: {
+            model: "TAG Heuer AQUARACER ALARM WBP1110.BA0627",
+            price: 661_500,
+            img: "images/tag-watch-2.jpg",
+            company: "tagheuer"
+        },
+        tagheuer_model_3: {
+            model: "TAG Heuer FORMULA 1 Watch WAZ1110.FT8023",
+            price: 425_500,
+            img: "images/tag-watch-3.jpg",
+            company: "tagheuer"
+        },
+        tagheuer_model_4: {
+            model: "TAG Heuer FORMULA 1 WATCH WAZ1120.BB0879",
+            price: 724_500,
+            img: "images/tag-watch-4.jpg",
+            company: "tagheuer"
+        }
+    },
+    rado: {
+        rado_model_1: {
+            model: "The Original Automatic R12995103",
+            price: 410_915,
+            img: "images/rado-watch-1.jpg",
+            company: "Rado"
+        },
+        rado_model_2: {
+            model: "Rado True Square Automatic Blue Dial Men’s Watch – 763.6077.3020",
+            price: 572_670,
+            img: "images/rado-watch-2.jpg",
+            company: "Rado"
+        },
+        rado_model_3: {
+            model: "Rado Ceramica Black Quartz Analog  Women’s Watch – R21702182",
+            price: 551_165,
+            img: "images/rado-watch-3.jpg",
+            company: "Rado"
+        },
+        rado_model_4: {
+            model: "   Rado Centrix Black Dial Automatic Men’s Watch – 763.0035.3071 ",
+            price: 602_015,
+            img: "images/rado-watch-4.jpg",
+            company: "Rado"
+        },
 
-// }
+    },
+    citizen: {
+        citizen_model_1: {
+            model: "PROMASTER MARINE – BN2036-14E",
+            price: 224_450,
+            img: "images/citizen-watch-1.jpg",
+            company: "Citizen"
+        },
+        citizen_model_2: {
+            model: "PROMASTER MARINE – NB6021-68L",
+            price: 334_350,
+            img: "images/citizen-watch-2.jpeg",
+            company: "Citizen"
+        },
+        citizen_model_3: {
+            model: "ECO-DRIVE CHRONOGRAPH – CA0773-15E",
+            price: 107_700,
+            img: "images/citizen-watch-3.jpg",
+            company: "Citizen"
+        },
+        citizen_model_4: {
+            model: "PROMASTER WORLD TIMER – CB5004-59W ",
+            price: 247_000,
+            img: "images/citizen-watch-4.jpg",
+            company: "Citizen"
+        },
 
-// function deltodo() {
-// alert("hello")
-// }
+    }
+    ,
+    tissot: {
+        rado_model_1: {
+            model: "TISSOT CARSON PREMIUM POWERMATIC 80",
+            price: 159_860,
+            img: "images/tissot-watch-1.jpg",
+            company: "Tissot"
+        },
+        rado_model_2: {
+            model: "TISSOT CARSON PREMIUM",
+            price: 81_250,
+            img: "images/tissot-watch-2.jpg",
+            company: "Rado"
+        },
+        rado_model_3: {
+            model: "TISSOT CHEMIN DES TOURELLES POWERMATIC 80 GMT",
+            price: 250_750,
+            img: "images/tissot-watch-3.jpg",
+            company: "Rado"
+        },
+        rado_model_4: {
+            model: "TISSOT EVERYTIME MEDIUM ",
+            price: 63_350,
+            img: "images/tissot-watch-4.jpg",
+            company: "Rado"
+        },
 
+    },
+    gshock: {
+        gshock_model_1: {
+            model: "Casio Analog-Digital Black Dial Women’s Watch-BA-110RG-1ADR (BX157)",
+            price: 32_715,
+            img: "images/gshock-watch-1.jpg",
+            company: "G-Shock"
+        },
+        gshock_model_2: {
+            model: "Casio G-Shock Ana-Digi 5900 Series Men’s Watch ? DW-5900-1DR",
+            price: 31_300,
+            img: "images/gshock-watch-2.jpg",
+            company: "G-Shock"
+        },
+        gshock_model_3: {
+            model: "Casio G-Shock Analog Digital Men’s Watch – GA-2000-1A2DR",
+            price: 42_400,
+            img: "images/gshock-watch-3.jpg",
+            company: "G-Shock"
+        },
+        gshock_model_4: {
+            model: "Casio G-Shock Analog Digital Men’s Watch – GA-2000-1A9DR",
+            price: 42_400,
+            img: "images/gshock-watch-4.jpg",
+            company: "G-Shock"
+        },
 
-function popup(){
-    var popupmain = document.getElementById("popup_main");
-    var main = document.getElementById("main")
-    popupmain.style.display = "flex";  
+    }
 }
 
-function post() {
-   var textbox = document.getElementById("textarea")
- var posts = document.getElementById("post-mains")
- var post_btn = document.getElementById("post_btn")
- var popupmain = document.getElementById("popup_main");
 
+for (var key in watch) {
+    for (var key1 in watch[key]) {
+     
+        
+// console.log(watch[key][key1][key2].img)
+            var main = document.getElementById("my-card")
+            main.innerHTML += `  
+      
+                
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3">
+                <div class="card p-0 product-card">
+                    <img src="${watch[key][key1].img}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-brand">${watch[key][key1].company}</p>
+                        <h6 class="card-title">${watch[key][key1].model}</h6>
+                        <span class="product-price">Rs: ${watch[key][key1].price}/- </span>
+                        <br />
+                    </div>
+                </div>
+                </div>
+                `
+          
+        }
 
-//  if(textbox.value === ""){
-//     post_btn.enabled = true;
-//  }
+    }
 
- posts.innerHTML += `<div class="first-main"><div class="post-head">
-   <div class="post-head-1"><img src="./img/profile.jpg" alt="" width="200px"></div>
-   <div class="post-head-2">
-     <p> Muhammad Faizan</p>
-   </div>
+    function all() {
+        for (var key in watch) {
+            for (var key1 in watch[key]) {
+             
+                
+                    var main = document.getElementById("my-card")
+                    main.innerHTML += `  
+              
+                        
+                    <div class="col-12 col-sm-12 col-md-6 col-lg-3">
+                        <div class="card p-0 product-card">
+                            <img src="${watch[key][key1].img}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-brand">${watch[key][key1].company}</p>
+                                <h6 class="card-title">${watch[key][key1].model}</h6>
+                                <span class="product-price">Rs: ${watch[key][key1].price}/- </span>
+                                <br />
+                            </div>
+                        </div>
+                        </div>
+                        `
+                  
+                }
+        
+            }
+    }
+    function filter(brands) {
+        for (var key in watch) {
+            for (var key1 in watch[key]) {
+              
+                    
+                    if(brands === key){
+                        var mains = document.getElementById("filter-cards")
+                       mains.innerHTML += `  
+                 
+                           
+                       <div class="col-12 col-sm-12 col-md-6 col-lg-3">
+                           <div class="card p-0 product-card">
+                               <img src="${watch[key][key1].img}" class="card-img-top" alt="...">
+                               <div class="card-body">
+                                   <p class="card-brand">${watch[key][key1].company}</p>
+                                   <h6 class="card-title">${watch[key][key1].model}</h6>
+                                   <span class="product-price">Rs: ${watch[key][key1].price}/- </span>
+                                   <br />
+                               </div>
+                           </div>
+                           </div>
+                           
+                           `
+                           
+                    }}
+                }
+  
+                main.innerHTML = ""
 
- </div>
- <div class="post-content">
-   <h2>${textbox.value}</h2>
- </div>
- <div class="flx-parent end">
-   <div class="icon-text icons  " id="pra" onclick ="color_chng()"><i class="fa-regular fa-thumbs-up"></i>
-     <p >Like</p>
-   </div>
-   <div class="icon-text icons"><i class="fa-regular fa-message"></i>
-     <p>Comment</p>
-   </div>
-   <div class="icon-text icons"><i class="fa-solid fa-share"></i>
-     <p>Share </p>
-   </div>
- </div>
- </div>
-`
-popupmain.style.display = "none";
-textbox.value = ""
+        }
+        
+                // if(brands == watch){
+            // var main = document.getElementById("my-card")
+            // main.innerHTML += `  
+      
+                
+            // <div class="col-12 col-sm-12 col-md-6 col-lg-3">
+            //     <div class="card p-0 product-card">
+            //         <img src="${watch[key][key1].img}" class="card-img-top" alt="...">
+            //         <div class="card-body">
+            //             <p class="card-brand">${watch[key][key1].company}</p>
+            //             <h6 class="card-title">${watch[key][key1].model}</h6>
+            //             <span class="product-price">Rs: ${watch[key][key1].price}/- </span>
+            //             <br />
+            //         </div>
+            //     </div>
+            //     </div>
+            //     `
+        // }
+    // }
 
-}
-function color_chng(){
-    var clr_chng = document.getElementById("pra")
-  clr_chng.style.color="#1b74e4"
-}
+     
